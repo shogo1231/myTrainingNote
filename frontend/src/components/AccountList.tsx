2b412c0bd99ebf3acad3704cc7001725a354db62
shogo1231 <shogo123198@gmail.com>
2023-02-11 16:09:33 +0900

    add カレンダー表示 FullCalendarライブラリを使ってカレンダー表示処理を作成する

--- frontend/src/components/AccountList.tsx
+++ frontend/src/components/AccountList.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from "./Header";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
+import jaLocale from '@fullcalendar/core/locales/ja';
+import interactionPlugin from "@fullcalendar/interaction";
 import '../App.css';
+import '../css/AccountList.css';
 
 interface Fruit {
   id: string;
@@ -34,6 +40,10 @@ const App = () => {
     );
   };
 
+  const handleDateClick = () => { // bind with an arrow function
+    alert('click')
+  }
+
   return (
     <>
       <Header />
@@ -43,6 +53,40 @@ const App = () => {
         {userDatas?.map((fruit: Fruit, index) => {
           return <Item key={index} name={fruit.name} ></Item>;
         })}
+      </div>
+      <div>
+      <FullCalendar
+        plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
+        initialView="dayGridWeek"
+        locales={[jaLocale]}
+        locale='ja'
+        dateClick={handleDateClick}
+        // weekends={true} // 週末を強調表示する。
+        titleFormat={{ // タイトルのフォーマット。
+          year: 'numeric',
+          month: 'short',
+        }}
+        // customButtons= {{ // カスタムボタン設置用
+        //   'change': {
+        //     text: '週表示',
+        //     click: function() {
+        //       alert('clicked the custom button!');
+        //     }
+        //   }
+        // }}
+        headerToolbar={{ // カレンダーのヘッダー設定。
+          start: 'title',
+          center: 'dayGridMonth,dayGridWeek',
+          right: 'prev,next today',
+        }}
+        buttonText={ {
+          today: '今日',
+          month: '月表示',
+          week: '週表示',
+          list: 'リスト'
+        }}
+        height={ 300 }
+      />
       </div>
     </>
   );
