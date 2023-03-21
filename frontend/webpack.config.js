04e8e1f46e5320adf0c1ae20c382a74c6fdc87f3
shogo1231 <shogo123198@gmail.com>
2023-03-21 15:40:27 +0900

    add 初期設定

---
+++ frontend/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/training',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      /* TypeScriptのモジュール */
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['@babel/preset-env', '@babel/react'] },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ]
+      },
+      /* CSSのモジュール */
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ]
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    // react-router-domで画面遷移する際の404エラー対策
+    // この設定について詳細確認が必要
+    historyApiFallback: true,
+    port: 3000,
+    // 「ドメイン名/api」のパスをバックエンドサーバーへ転送する設定
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001'
+      }
+    },
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  target: 'web',
+};
