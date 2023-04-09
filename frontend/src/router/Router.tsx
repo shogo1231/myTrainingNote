6fb5e61e4e8e0f0e84ac3e42ceb1b6df5546d4c0
shogo1231 <shogo123198@gmail.com>
2023-04-09 13:36:11 +0900

    fix URI変更　不要コード削除

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,4 +1,4 @@
-import AccountList from '../components/AccountList';
+import Top from '../components/Top';
 import TrainingLog from '../components/TrainingLog';
 import SelectEvent from '../components/SelectEvent';
 import Register from '../components/Register';
@@ -11,7 +11,7 @@ const Router = () => {
   return  (
     <BrowserRouter>
       <Routes>
-        <Route path="/training/accountlist" element={<AccountList />} />
+        <Route path="/training/top" element={<Top />} />
         <Route path="/training/Log" element={<TrainingLog />} />
         <Route path="/training/selectEvent" element={<SelectEvent />} />
         <Route path="/training/register" element={<Register />} />
