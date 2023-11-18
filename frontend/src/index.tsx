6c2903cbd978040eaa30a434fb591c56df6e77aa
shogo1231 <shogo123198@gmail.com>
2023-11-18 20:09:49 +0900

    add favicon追加 サイトのfaviconを追加。追加中に備忘として残したい内容もついでに挙げておく

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,23 @@ import  Router from "./router/Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+  );
+  root.render(
+    <React.StrictMode>
     <Router />
   </React.StrictMode>
 );
+
+// PNG画像の読み込ませ方サンプル
+// 不要だがサンプルとして残しておく
+// const imageSrc = require('./images/favicon.png')
+// const imgDOM = ReactDOM.createRoot(
+//   document.getElementById('image') as HTMLElement
+// );
+// imgDOM.render(
+//   <React.StrictMode>
+//     <div>
+//       <img src={imageSrc}></img>
+//     </div>
+//   </React.StrictMode>
+// );
