4b9c89d81d93f4f024c66a727175b9495938fd79
shogo1231 <shogo123198@gmail.com>
2023-12-10 11:50:31 +0900

    verification　dist公開

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, './dist/training'),
     publicPath: '/training',
     filename: 'bundle.js',
     // assetModuleFilename: 'assets/[hash][ext][query]'
